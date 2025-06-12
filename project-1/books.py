fd833d272b50382c6997be5b99667ffbddcad46c
shinas07 <shinasamanpk@gmail.com>
2025-06-12 08:57:32 +0530

    feat: add delete http method

--- project-1/books.py
+++ project-1/books.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,status
+from typing import List
 
 app = FastAPI()
 
@@ -8,10 +9,33 @@ BOOKS = [
     {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
     {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
     {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
-]
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'},
+    {'title': 'Title Seven', 'author': 'Author One', 'category': 'history'},
 
+    {'title': 'Title Eight', 'author': 'Author Three', 'category': 'science'}
+]
 
 @app.get('/')
 async def read_all_books():
     return BOOKS
+
+
+@app.delete('/books/delete-book/{book_title}',status_code=status.HTTP_200_OK)
+async def delete_books(book_title : str):
+    book_title = book_title.casefold()
+
+    for book in BOOKS:
+        if book.get('title','').casefold() == book_title:
+            BOOKS.remove(book)
+            return {"message":f"Book '{book_title}' deleted successfully"}
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Book with title '{book_title}' not found."
+
+    )
+
+
+
+            
+            
